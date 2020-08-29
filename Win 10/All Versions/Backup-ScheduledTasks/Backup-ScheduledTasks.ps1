3ba53604e64b0e1e9a7e4c31b2b7d0f35fd74e41
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2020-08-29 12:41:32 +0200

    Backup Windows Schedule Tasks via PowerShell
    All needed information given inside the script.

---
+++ Win 10/All Versions/Backup-ScheduledTasks/Backup-ScheduledTasks.ps1
@@ -0,0 +1,21 @@
+﻿# Set-ExecutionPolicy Unrestricted
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force -Scope CurrentUser
+
+
+# Starting the script
+# .\Backup-ScheduledTasks.ps1
+$PATH = "C:\PoSh\Save_ScheduledTasks"
+if (!(Test-Path $PATH)) {New-Item -Path $PATH -ItemType Directory}
+
+$sch = New-Object -ComObject("Schedule.Service")
+$sch.Connect("localhost")
+$tasks = $sch.GetFolder("\").GetTasks(0)
+
+$outfile_temp = "C:\PoSh\Save_ScheduledTasks\{0}.xml";
+
+$tasks | %{
+    $xml = $_.Xml
+    $task_name = $_.Name
+    $outfile = $outfile_temp -f $task_name
+    $xml | Out-File $outfile
+}
